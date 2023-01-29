5ce507506a1efdf66762ac1f8b2bae63df3b1e28
Leila Schmidt <le.schmidt@douglas.de>
2023-01-29 17:49:53 +0100

    Implementation of the quicksort algorithm, but this time instead of shuffling through the array, we create two new arrays each time for numbers higher and lower than the pivot value.

--- NumQuickSort/Program.cs
+++ NumQuickSort/Program.cs
@@ -1,10 +1,70 @@
-﻿namespace NumQuickSort
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NumQuickSort
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> numbers = new List<int>();
+
+            while (true)
+            {
+                Console.WriteLine("Enter a number or 'stop' to end: ");
+                var userInput = Console.ReadLine();
+                if (userInput.Trim().ToLower() == "stop")
+                {
+                    break;
+                }
+                if (!int.TryParse(userInput, out int numUserInput))
+                {
+                    Console.WriteLine("This is not a number");
+                    continue;
+                }
+                numbers.Add(numUserInput);
+            }
+            Console.WriteLine("The numbers you entered were... ");
+            Console.WriteLine(string.Join(", ", numbers));
+
+            QuickSort(numbers, 0, numbers.Count - 1);
+            // Print the numbers in ascending order
+            Console.WriteLine("Numbers in ascending order: " + string.Join(", ", numbers));
+
+        }
+
+        static List<int> QuickSort(List<int> arr, int low, int high)
+        {
+            if (low < high)
+            {
+                List<int> left = new List<int>();
+                List<int> right = new List<int>();
+
+                int pivot = arr[high];
+
+                for (int i = low; i < high; i++)
+                {
+                    if (arr[i] <= pivot)
+                    {
+                        left.Add(arr[i]);
+                    }
+                    else
+                    {
+                        right.Add(arr[i]);
+                    }
+                }
+
+                // Recursively sort the left and right parts of the pivot
+                left = QuickSort(left, 0, left.Count - 1);
+                right = QuickSort(right, 0, right.Count - 1);
+
+                // Merge the left and right arrays back into the original array
+                arr.Clear();
+                arr.AddRange(left);
+                arr.Add(pivot);
+                arr.AddRange(right);
+            }
+
+            return arr;
         }
     }
 }
